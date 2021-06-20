2a679abd7f7af77dfa59719448176498724448af
pravinv1998 <pravin.wargad1998@gmail.com>
2021-06-20 23:41:35 +0530

    test api

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+// export async function data() {
+//   const res = await fetch(`${API_BASE_URL}${POST_URL}`);
+//   return await res.json();
+// }
+
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name: "pravin" });
 }
